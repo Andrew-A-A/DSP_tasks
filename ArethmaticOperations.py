b7343bec16f4e6bf7a345ce433f7a4fe35ace042
Andrew Adel <andrew.adel.a@outlook.com>
2023-12-19 07:19:53 +0200

    Add comments to ArithmeticOperations.py

--- ArethmaticOperations.py
+++ ArethmaticOperations.py
@@ -1,5 +1,8 @@
 def add(signals_to_add):
+    # Initialize the total with the first signal
     total = signals_to_add[0]
+
+    # Iterate over the remaining signals and add their values to the total
     for signal in signals_to_add[1:]:
         i = 0
         for index, val in signal:
@@ -10,8 +13,11 @@ def add(signals_to_add):
 
 
 def subtract(signal1, signal2):
-    i = 0
+    # Initialize the total with the first signal
     total = signal1
+
+    # Iterate over the values of the second signal and subtract them from the total
+    i = 0
     for index, val in signal2:
         total[i][1] = val - total[i][1]
         i += 1
@@ -19,6 +25,7 @@ def subtract(signal1, signal2):
 
 
 def multiply(signal, value):
+    # Multiply each value in the signal by the given value
     i = 0
     total = signal
     for _, _ in total:
@@ -28,6 +35,7 @@ def multiply(signal, value):
 
 
 def square(signal):
+    # Square each value in the signal
     i = 0
     total = signal
     for _, value in total:
@@ -37,18 +45,20 @@ def square(signal):
 
 
 def shift(signal, unit):
+    # Shift the time values of the signal by the given unit
     ret = signal
-    for indx in range(len(ret)):
-        ret[indx][0] += unit
+    for index in range(len(ret)):
+        ret[index][0] += unit
     return ret
 
 
-def normalize(signal, range):
+def normalize(signal, Range):
+    # Normalize the amplitudes of the signal to the specified range
     Min = min([inner_list[1] for inner_list in signal])
     Max = max([inner_list[1] for inner_list in signal])
-    scaling_factor = (range[1] - range[0]) / (Max - Min)
+    scaling_factor = (Range[1] - Range[0]) / (Max - Min)
     result = [
-        [inner_list[0], (inner_list[1] - Min) * scaling_factor + range[0]]
+        [inner_list[0], (inner_list[1] - Min) * scaling_factor + Range[0]]
         for inner_list in signal
     ]
 
@@ -56,6 +66,7 @@ def normalize(signal, range):
 
 
 def accumulation(signal):
+    # Compute the cumulative sum of the signal values
     y = signal
     for k in range(1, len(signal)):
         y[k][0] = y[k - 1][0] + signal[k][0]
